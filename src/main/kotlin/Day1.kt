d11db595db518ea74debcb4a7f1d750c2fd8ba82
Jonathan <jonfritz@gmail.com>
2024-12-03 19:49:35 -0500

    Day 1

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,19 +1,31 @@
 package ca.jonathanfritz.aoc2k4
 
+import kotlin.math.abs
+
 class Day1 {
 
     fun part1(input: List<String>): Int {
-        input.map { println(it) }
-        return 0
+        val integers = input.map { line -> line.split(" ").filter { token -> token.isNotBlank() } }
+            .map { it[0].toInt() to it[1].toInt() }
+        val left = integers.map { it.first }.sorted()
+        val right = integers.map { it.second }.sorted()
+        return left.mapIndexed { i, token ->
+            abs(token - right[i])
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val integers = input.map { line -> line.split(" ").filter { token -> token.isNotBlank() } }
+            .map { it[0].toInt() to it[1].toInt() }
+        val keys = integers.map { it.first }
+        val values = integers.map { it.second }
+        val counts = keys.associateWith { key -> values.count { it == key } }
+        return keys.sumOf { it * (counts[it] ?: 0) }
     }
 }
 
 fun main() {
-    val input = Utils.loadFromFile("Day1/test.txt")
+    val input = Utils.loadFromFile("Day1/input.txt")
     val day1 = Day1()
     println("Part 1: ${day1.part1(input)}")
     println("Part 2: ${day1.part2(input)}")
