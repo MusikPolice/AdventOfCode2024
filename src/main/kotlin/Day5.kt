f2e74284e3a0792fb5fade243e17db8d74a371a1
Jonathan <jonfritz@gmail.com>
2024-12-06 07:03:01 -0500

    Day 5, Part 2

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -14,7 +14,18 @@ class Day5 {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val rules = input.parseRules()
+        val updates = input.parseUpdates()
+        val incorrectUpdates = updates.filterNot {
+            it.satisfiesRules(rules)
+        }
+
+        // re-order each incorrect update such that it satisfies the rules and return the sum of middle elements
+        return incorrectUpdates.map {
+            it.sort(rules)
+        }.sumOf {
+            it.elementAt(it.size/2)
+        }
     }
 
     private fun List<Int>.satisfiesRules(rules: Map<Int, Set<Int>>): Boolean {
@@ -46,6 +57,58 @@ class Day5 {
         }
 
         return true
+    }
+
+    private fun List<Int>.sort(rules: Map<Int, Set<Int>>): List<Int> {
+        // we only care to evaluate rules that apply to the pages in this update
+        val applicableRules = rules.filter {
+                rule -> rule.key in this || rule.value.intersect(this.toSet()).isNotEmpty()
+        }
+
+        var isSorted = false
+        val update = this.toMutableList()
+        while (!isSorted) {
+
+            // let's assume that the list is sorted until we find that it isn't
+            isSorted = true
+
+            (update.indices).forEach { i ->
+                // is it before everything that is supposed to come after it?
+                val previous = update.subList(0, i)
+                if (previous.isNotEmpty()) {
+                    val mustComeBefore = applicableRules.getOrDefault(update[i], emptySet())
+                    val intersection = previous.intersect(mustComeBefore)
+                    if (intersection.isNotEmpty()) {
+                        // the list is not correctly sorted, move the element at i to the place before the first
+                        // element in the intersection
+                        isSorted = false
+                        val toMove = update[i]
+                        val target = update.indexOf(intersection.first())
+                        update.removeAt(i)
+                        update.add(target, toMove)
+                    }
+                }
+
+                // is it after everything that is supposed to come before it?
+                val subsequent = update.subList(i + 1, update.size)
+                if (subsequent.isNotEmpty()) {
+                    val mustComeAfter = applicableRules.filter {
+                        rule -> rule.value.contains(update[i])
+                    }.map { rule -> rule.key }.toSet()
+                    val intersection = subsequent.intersect(mustComeAfter)
+                    if (intersection.isNotEmpty()) {
+                        // the list is not correctly sorted, move the element at i to the end of the list
+                        isSorted = false
+                        val toMove = update[i]
+                        val target = update.indexOf(intersection.last())
+                        update.removeAt(i)
+                        update.add(target,toMove)
+                    }
+                }
+            }
+        }
+
+        return update
     }
 
     private fun List<String>.parseRules(): Map<Int, Set<Int>> {
